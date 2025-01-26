8350ea0d7922615e75d439e981460ebcfcbb8fc0
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2025-01-26 15:48:06 +0000

    Sync LeetCode submission Runtime - 7 ms (7.56%), Memory - 8 MB (100.00%)

---
+++ 0009-palindrome-number/solution.c
@@ -0,0 +1,14 @@
+bool isPalindrome(int x) {
+    if(x<0 || x!=0 && x%10==0)return false;
+    long long reversed=0;
+    
+    long long original=x;
+    while (x!=0){
+       
+        reversed=reversed*10+x%10;
+        x/=10;
+    }
+    return original==reversed;
+}
+
+
