7044b5c674e9a885166331e85a290a1b08b04686
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2025-07-19 13:48:59 +0000

    Sync LeetCode submission Runtime - 10 ms (26.08%), Memory - 40.2 MB (99.99%)

---
+++ 1013-fibonacci-number/solution.java
@@ -0,0 +1,28 @@
+// class Solution {
+//     public int fib(int n) {
+//         if(n==1) {
+//             return 1;}
+
+//         else if(n==0) {
+//             return 0;}
+//         else {
+//             return fab(n-1)+fab(n-2);
+//         }
+        
+        
+//     }
+// }
+class Solution {
+    public int fib(int n) {
+        
+        if ( n==0){
+            return 0;
+        }
+        if (n==1) {
+            return 1;
+        }
+        else{
+            return fib(n-1) + fib(n-2);
+        }
+    }
+}
