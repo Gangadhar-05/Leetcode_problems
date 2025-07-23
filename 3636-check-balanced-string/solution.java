a654d86380ca63c93136cb376197e8375d19b3a7
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2025-07-23 13:41:42 +0000

    Sync LeetCode submission Runtime - 1 ms (97.99%), Memory - 42.4 MB (99.87%)

---
+++ 3636-check-balanced-string/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int evensum=0;
+        int oddsum=0;
+        for(int i=0;i<num.length();i++){
+            int digit=num.charAt(i)-'0';
+            if(i%2==0) evensum+=digit;
+            else oddsum+=digit;
+            
+        }if(evensum==oddsum)return true;
+        else return false;
+    }
+}
