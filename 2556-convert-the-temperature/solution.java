fc9b9f68d1e1b825b4b2d90b73a9b1ff751e2133
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2025-07-25 15:33:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.3 MB (100.00%)

---
+++ 2556-convert-the-temperature/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double kelvin=celsius+273.15;
+        double fahrenheit=(celsius*1.80)+32.00;
+        double[] d=new double[2];
+        d[0]=kelvin;
+        d[1]=fahrenheit;
+        return d;
+}
+}
