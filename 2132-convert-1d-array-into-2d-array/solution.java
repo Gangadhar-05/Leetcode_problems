49f914990093cc9bf812833bd66ffff5cb2539fb
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2025-08-13 10:42:21 +0000

    Sync LeetCode submission Runtime - 6 ms (4.46%), Memory - 55.6 MB (100.00%)

---
+++ 2132-convert-1d-array-into-2d-array/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int k=0;
+        int[][] arr=new int[m][n];
+        if(m*n!=original.length) return new int[0][0] ;
+        else{
+       
+            for(int i=0;i<original.length;i++){
+                
+                    arr[i/n][i%n]=original[i];
+                    k++;
+                
+            }
+        }
+        
+        return arr;
+    }
+}
