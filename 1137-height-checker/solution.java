b6fc81da57b039c71d0dca6acd8c8a46018b09bb
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2025-08-17 16:27:21 +0000

    Sync LeetCode submission Runtime - 2 ms (90.42%), Memory - 41.8 MB (99.97%)

---
+++ 1137-height-checker/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+       int[] exp=new int[heights.length];
+       for(int i=0;i<heights.length;i++){
+        exp[i]=heights[i];
+       } 
+       Arrays.sort(heights);
+       int cnt=0;
+       for(int i=0;i<heights.length;i++){
+        if(heights[i]!=exp[i]){
+            cnt++;
+        }
+       }
+       return cnt;
+    }
+}
