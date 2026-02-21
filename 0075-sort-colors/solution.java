869eda3516f0d6e47f418fea5a059d0f4727f009
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2026-02-21 13:51:53 +0000

    Sync LeetCode submission Runtime - 1 ms (20.17%), Memory - 43.5 MB (40.96%)

---
+++ 0075-sort-colors/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+       for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+                int temp=nums[i];
+                nums[i]=nums[i-1];
+                nums[i-1]=temp;
+            }
+       } 
+          for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+                sortColors(nums);
+            }
+          }
+    }
+}
