78848eb491adbdeb992229fcef38523181bbde91
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2026-02-22 09:07:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.6 MB (41.17%)

---
+++ 1580-shuffle-the-array/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] arr=new int[2*n];
+        int j=0;
+        for(int i=0;i<2*n && j<n;i+=2){
+            arr[i]=nums[j++];
+        }
+        j=n;
+        for(int i=1;i<2*n && j<2*n;i+=2){
+            arr[i]=nums[j++];
+        }
+        return arr;
+    }
+}
