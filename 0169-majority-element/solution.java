d3a4197c1bd7c5b8d47c02e63f26f83c953ff650
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2026-06-25 07:05:40 +0000

    Sync LeetCode submission Runtime - 17 ms (16.58%), Memory - 52.7 MB (80.62%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,6 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int n=nums.length;
+        HashMap<Integer,Integer> freq=new HashMap<>();
+        for(int i=0;i<n;i++){
+            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
+            if(freq.get(nums[i])>n/2){
+                return nums[i];
+            }
+        }
+      
+        return -1;
     }
 }
