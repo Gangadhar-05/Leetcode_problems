63e2e553e9ac184b9cf036dc45efc310c0ad25eb
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2026-08-05 10:30:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43 MB (66.61%)

--- 0909-stone-game/solution.java
+++ 0909-stone-game/solution.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
         int n=piles.length;
-        int alice=0;
-        int bob=0;
-        int i=0;
-        int j=n-1;
-        while(i<=n/2){
-            alice+=Math.max(piles[i],piles[j]);
-            bob+=Math.min(piles[i],piles[j]);
-            i++;
-            j--;
-        }
+        // int alice=0;
+        // int bob=0;
+        // int i=0;
+        // int j=n-1;
+        // while(i<=n/2){
+        //     alice+=Math.max(piles[i],piles[j]);
+        //     bob+=Math.min(piles[i],piles[j]);
+        //     i++;
+        //     j--;
+        // }
        
-        if(alice>bob)return true;
-        else return false;
+        // if(alice>bob)return true;
+        // else return false;
+        int es = 0,os = 0;
+        for(int i = 0;i < n;i++){
+            if(piles[i] % 2 == 0) es+=piles[i];
+            else os+=piles[i];
+        }
+        return (es != os);
     }
 }
