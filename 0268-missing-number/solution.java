b2dbac20a4293d2fac7f2a8365c0945c1b40fe58
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2026-08-09 14:56:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.3 MB (48.81%)

--- 0268-missing-number/solution.java
+++ 0268-missing-number/solution.java
@@ -1,14 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-        int[] freq=new int[n+1];
-        for(int i=0;i<n;i++){
-            freq[nums[i]]++;
+        int tsum=n*(n+1)/2;
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
         }
-        for(int i=0;i<n+1;i++){
-            if(freq[i]==0)return i;
-        }
-        return -1;
+        return tsum-sum;
 
     }
 }
