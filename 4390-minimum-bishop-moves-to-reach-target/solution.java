09c0ca22748c8d43451040ec94647eedf9071b77
Bathula Gangadhar <gangadhar.2k24@gmail.com>
2026-08-29 15:21:43 +0000

    Sync LeetCode submission Runtime - 1 ms (95.02%), Memory - 44.3 MB (29.42%)

---
+++ 4390-minimum-bishop-moves-to-reach-target/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minBishopMoves(int[] source, int[] target) {
+        int x1=source[0];
+        int y1=source[1];
+        int x2=target[0];
+        int y2=target[1];
+        if((x1+y1)%2!=(x2+y2)%2){
+            return -1;
+        }else if(Math.abs(x1-x2)==Math.abs(y1-y2)){
+            return 1;
+        }
+        return 2;
+    }
+}
